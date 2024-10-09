b00707a8d59872b58476429419c6c534f763f6bb
megha thakre <megha1234567891@gmail.com>
2024-10-09 17:15:11 +0530

    main project

---
+++ dash.js
@@ -0,0 +1,8 @@
+function showside(){
+let selectside=document.querySelector('.sidebar');
+selectside.style.marginLeft="0px" ;
+selectside.style.transition="margin-left .5s";
+
+let selectsmallaside=document.querySelector('.smallsidebar');
+selectsmallaside.style.display="none";
+}
